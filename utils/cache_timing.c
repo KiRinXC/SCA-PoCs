9a241772721917cb5c70e281184ac4c90e5a6865
KiRinXC <guowenquan24@mails.ucas.ac.cn>
2025-07-30 09:55:31 -0400

    init

--- utils/cache_timing.c
+++ utils/cache_timing.c
@@ -6,7 +6,7 @@
 #define ARRAY_SIZE 1024 * 1024 * 16   // 16 MB
 #define CACHE_LINE_SIZE 64           // Typical cache line size in bytes
 #define TEST_ITERATIONS 10000        // Number of iterations for measuring
-
+#define ACCESS_COUNT ARRAY_SIZE / (CACHE_LINE_SIZE * 1024)
 // Function to calculate statistics
 void calculate_statistics(uint64_t *times, int count, uint64_t *min, uint64_t *max, double *avg) {
     *min = times[0];
@@ -39,26 +39,31 @@ int main() {
     unsigned int aux;
     uint64_t start_cycles, end_cycles;
     volatile char temp; // To prevent compiler optimization
+    temp = array[0];
     uint64_t hit_times[TEST_ITERATIONS]; // Cache hit times
     uint64_t miss_times[TEST_ITERATIONS]; // Cache miss times
 
     // Measure cache hit time multiple times
     for (int i = 0; i < TEST_ITERATIONS; i++) {
+        _mm_lfence();
         start_cycles = __rdtscp(&aux); // Start reading the timestamp
         temp = array[0];               // Access memory (cache hit expected)
         end_cycles = __rdtscp(&aux);   // End reading the timestamp
+        _mm_lfence();
         hit_times[i] = end_cycles - start_cycles; // Record the time
     }
 
     // Measure cache miss time multiple times
     for (int i = 0; i < TEST_ITERATIONS; i++) {
+        _mm_clflush((const void *)&array[0]);
+        _mm_lfence();
         start_cycles = __rdtscp(&aux); // Start reading the timestamp
-        for (uint64_t j = 0; j < ARRAY_SIZE; j += CACHE_LINE_SIZE * 1024) {
-            temp = array[j];           // Access sparse memory locations (cache miss expected)
-        }
+        temp = array[0];
         end_cycles = __rdtscp(&aux);   // End reading the timestamp
-        miss_times[i] = end_cycles - start_cycles; // Record the time
+        _mm_lfence();
+        miss_times[i] = (end_cycles - start_cycles); // Record the time
     }
+
 
     // Calculate statistics for cache hit
     uint64_t hit_min, hit_max;
